a838f674e8872efc12e8074e84e9633a3185bab5
Nathan Lee <leen7@spu.edu>
2020-01-24 22:29:24 -0800

    added basics elemsnts and calculations for page 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,71 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
+
+    nextBtn();
+    calculate();
 }
 
-MainWindow::~MainWindow()
-{
+void MainWindow::calculate(){
+    nextBtn();
+    double cost = 0;
+    if(ui->kingBtn->isChecked() && ui->atriumBtn->isChecked()){
+        cost += KING_ATRIUM;
+    }else if(ui->queenBtn->isChecked() && ui->atriumBtn->isChecked()){
+        cost += QUEEN_ATRIUM;
+    }else if(ui->kingBtn->isChecked() && ui->standardBtn->isChecked()){
+        cost += KING_STANDARD;
+    }else if(ui->queenBtn->isChecked() && ui->standardBtn->isChecked()){
+        cost += QUEEN_STANDARD;
+    }
+    if(ui->parkingBtn->isChecked()){
+        cost += 12.75;
+    }
+    cost *= ui->numDays->value();
+    ui->costLbl->setText("Price: " + QString::number(cost));
+}
+
+void MainWindow::nextBtn(){
+    bool goNext = true;
+    if(!ui->kingBtn->isChecked() && !ui->queenBtn->isChecked()){
+        goNext = false;
+    }else if(!ui->atriumBtn->isChecked() && !ui->standardBtn->isChecked()){
+        goNext = false;
+    }else if(!ui->parkingBtn->isChecked()){
+        goNext = false;
+    }else if(ui->numAdultsBox->value() == 0){
+        goNext = false;
+    }
+    ui->nextBtn->setDisabled(!goNext);
+
+}
+
+MainWindow::~MainWindow(){
     delete ui;
 }
 
+void MainWindow::on_queenBtn_clicked(){
+    calculate();
+}
+
+void MainWindow::on_kingBtn_clicked(){
+    calculate();
+}
+
+void MainWindow::on_atriumBtn_clicked(){
+    calculate();
+}
+
+void MainWindow::on_standardBtn_clicked(){
+    calculate();
+}
+
+void MainWindow::on_parkingBtn_clicked(){
+    calculate();
+}
+
+void MainWindow::on_numDays_valueChanged(int arg1){
+    calculate();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,13 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+const int KING_ATRIUM = 350;
+const int KING_STANDARD = 290;
+const int QUEEN_ATRIUM = 325;
+const int QUEEN_STANDARD= 284;
+const double PARKING = 12.75;
+
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -14,6 +21,23 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void calculate();
+
+    void nextBtn();
+
+    void on_queenBtn_clicked();
+
+    void on_kingBtn_clicked();
+
+    void on_atriumBtn_clicked();
+
+    void on_standardBtn_clicked();
+
+    void on_parkingBtn_clicked();
+
+    void on_numDays_valueChanged(int arg1);
 
 private:
     Ui::MainWindow *ui;
